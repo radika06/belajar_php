2f8264dab363ba7cdc8ea330c467ec115cd6c078
radika06 <Radzz900@gmail.com>
2022-09-16 17:27:10 +0700

    fix1 index

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+$nama1 ="Dede Belajar PHP";
+$nama2 ="PHP";
+$cari = strpos($nama1 , $nama2);
+echo $cari;
+// echo strtoupper($nama1);
+// echo strtolower($nama2);
+// $output = strcmp($nama1, $nama2);
+
+// if ($output !== 0) {
+// 	echo "Namanya tidak sama";
+// 	# code...
+// }
+// else{ 
+// 	echo "Namanya sama";
+// }
+ 	 ?>
