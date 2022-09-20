71d7d9a9346a97998024abe111789ac819a43d9a
radika06 <Radzz900@gmail.com>
2022-09-20 16:45:36 +0700

    Fixing1

---
+++ tugas/mtk.php
@@ -0,0 +1,42 @@
+<?php 
+// $panjang = 12;
+// $lebar = 3;
+// $luas = $panjang * $lebar;
+
+// echo $luas;
+// $tambahdua = 2;
+// $tambahdua += 2;
+// $tambahdua = $tambahdua +2
+// echo $tambahdua;
+//
+// $sifat = "Rajin";
+// var_dump($sifat == "Rajin");
+// var_dump(true || false);
+//
+// $angka1 = 25;
+// $angka2 = 45;
+// if ($angka1 == 25 || $angka2 == 47) {
+// 	echo "kondisi benar";
+// 	# code...
+// }
+// else {
+// 	echo "kondisi salah";
+// }
+
+// 
+// function kenalkan($nama){
+// 	echo "kenalkan namanya" . $nama;
+// } 
+// kenalkan ("dede");
+
+// function buat_nama_kapital($nama, $asal)
+// {
+// 	return ucwords($nama);
+// }
+// $nama_lengkap = buat_nama_kapital("dede radika", "bogor");
+// echo $nama_lengkap;
+
+// for ($i=6; $i < 1; $i++) {
+// 	echo "ini adalah looping ke $i <br>";
+// }
+ ?>
